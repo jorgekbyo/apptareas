59a0bbe2779a273dd8fe305b38519173d45e079f
jorgekbyo <jorgegonzalezcabello@gmail.com>
2017-04-03 08:23:06 +0200

    ENTREGA FASE 1

---
+++ routes/tareas.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+var tareas = [
+	{
+		"nom":"Diseño wireframe",
+		"etiquetes": [ "wireframe","app","hibrida"],
+		"realització":"36"
+	},
+	{
+		"nom":"Reunión",
+		"etiquetes": ["app","hibrida"],
+		"realització":"36"
+	},
+	{
+		"nom":"Desarrollo Layout",
+		"etiquetes": [ "layout","app","hibrida"],
+		"realització":"36"
+	},
+	{
+		"nom":"Reunión cliente",
+		"etiquetes": [ "reunión","app","hibrida"],
+		"realització":"36"
+	}
+]
+router.get('/', function(req, res, next) {
+  res.render('tareas', { title: 'appJorge', tareas: tareas });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'appJorge'});
+});
+
+
+module.exports = router;
